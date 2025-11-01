ccfb58aa5b4400b4e5ba3eda0096dce945bdd026
Ariane Horbach <arianehorabch@googlemail.com>
2025-11-01 19:39:42 +0100

    [OTEL] Refactored Code into Packages and proper seperation of concerns

--- ScalaInstrumentation/src/main/scala/Main.scala
+++ ScalaInstrumentation/src/main/scala/Main.scala
@@ -1,11 +1,22 @@
 package com.example
 
+import spark.DataFrameProcessor
+
 /**
  * Application entry point.
  */
 object Main {
+
   def main(args: Array[String]): Unit = {
-    println("Processing Spark DataFrame with OpenTelemetry tracing...")
-    DataFrameProcessor.processEmployeeData()
+    println("Processing Spark DataFrame...")
+
+    try {
+      // Call business logic
+      DataFrameProcessor.processEmployeeData()
+
+    } catch {
+      case ex: Exception =>
+        throw ex
+    }
   }
 }
